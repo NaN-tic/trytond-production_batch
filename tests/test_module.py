489b938955c2052daf2ecebfe90a4d4b07ca1459
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2026-04-07 10:11:00 +0200

    FIX tests: - Add missing phantom related fields, since: https://github.com/tryton/tryton/commit/3d99b80027f84499a2e1a469d4e7507130f86051 - Add missnig company to work with the production.work.center. - Change the way we handle the production.bom and production.bom.output. Generate the outputs at the same time as the bom, and not after.

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,7 +2,8 @@
 # repository contains the full copyright notices and license terms.
 
 #from trytond.exceptions import UserError
-from trytond.modules.company.tests import CompanyTestMixin
+from trytond.modules.company.tests import (
+    CompanyTestMixin, create_company, set_company)
 from trytond.pool import Pool
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 
@@ -16,64 +17,102 @@ class ProductionBatchTestCase(CompanyTestMixin, ModuleTestCase):
         "Production on_change_product uses the lowest sequence product BOM"
         pool = Pool()
         Uom = pool.get('product.uom')
-        Product = pool.get('product.product')
+        Template = pool.get('product.template')
         ProductBom = pool.get('product.product-production.bom')
         Production = pool.get('production')
         Bom = pool.get('production.bom')
-        BomOutput = pool.get('production.bom.output')
         Routing = pool.get('production.routing')
         WorkCenterCategory = pool.get('production.work.center.category')
         WorkCenter = pool.get('production.work.center')
 
-        unit, = Uom.search([('name', '=', 'Unit')], limit=1)
-        product = Product(default_uom=unit)
+        company = create_company()
 
-        bom_1 = Bom(
-            name='BOM 1',
-            inputs=[],
-            outputs=[BomOutput(product=product, quantity=1, unit=unit)])
-        bom_2 = Bom(
-            name='BOM 2',
-            inputs=[],
-            outputs=[BomOutput(product=product, quantity=1, unit=unit)])
-        routing_1 = Routing(name='Routing 1')
-        routing_2 = Routing(name='Routing 2')
-        category = WorkCenterCategory(name='Line Category', is_line=True)
-        line_1 = WorkCenter(name='Line 1', category=category)
-        line_2 = WorkCenter(name='Line 2', category=category)
+        with set_company(company):
+            unit, = Uom.search([('name', '=', 'Unit')], limit=1)
+            template, = Template.create([{
+                        'name': 'Batch Product',
+                        'type': 'goods',
+                        'default_uom': unit.id,
+                        'producible': True,
+                        'products': [('create', [{}])],
+                        }])
+            product, = template.products
 
-        product.boms = [
-            ProductBom(
-                product=product,
-                sequence=20,
-                bom=bom_2,
-                routing=routing_2,
-                line=line_2,
-                batch_quantity=10,
-                batch_uom=unit),
-            ProductBom(
-                product=product,
-                sequence=10,
-                bom=bom_1,
-                routing=routing_1,
-                line=line_1,
-                batch_quantity=5,
-                batch_uom=unit),
-            ]
+            bom_1, bom_2 = Bom.create([{
+                        'name': 'BOM 1',
+                        'phantom': False,
+                        'inputs': [],
+                        'outputs': [('create', [{
+                                        'product': product.id,
+                                        'quantity': 1,
+                                        'unit': unit.id,
+                                        }])],
+                        }, {
+                        'name': 'BOM 2',
+                        'phantom': False,
+                        'inputs': [],
+                        'outputs': [('create', [{
+                                        'product': product.id,
+                                        'quantity': 1,
+                                        'unit': unit.id,
+                                        }])],
+                        }])
+            routing_1, routing_2 = Routing.create([{
+                        'name': 'Routing 1',
+                        }, {
+                        'name': 'Routing 2',
+                        }])
+            Routing.write([routing_1], {
+                    'boms': [('add', [bom_1.id])],
+                    })
+            Routing.write([routing_2], {
+                    'boms': [('add', [bom_2.id])],
+                    })
+            category, = WorkCenterCategory.create([{
+                        'name': 'Line Category',
+                        'is_line': True,
+                        }])
+            line_1, line_2 = WorkCenter.create([{
+                        'name': 'Line 1',
+                        'category': category.id,
+                        'company': company.id,
+                        }, {
+                        'name': 'Line 2',
+                        'category': category.id,
+                        'company': company.id,
+                        }])
 
-        production = Production()
-        production.state = 'draft'
-        production.product = product
+            ProductBom.create([{
+                        'product': product.id,
+                        'sequence': 20,
+                        'bom': bom_2.id,
+                        'routing': routing_2.id,
+                        'line': line_2.id,
+                        'batch_quantity': 10,
+                        'batch_uom': unit.id,
+                        }, {
+                        'product': product.id,
+                        'sequence': 10,
+                        'bom': bom_1.id,
+                        'routing': routing_1.id,
+                        'line': line_1.id,
+                        'batch_quantity': 5,
+                        'batch_uom': unit.id,
+                        }])
 
-        production.on_change_product()
+            production = Production()
+            production.state = 'draft'
+            production.product = product
 
-        # self.assertEqual(production.unit, unit)
-        # self.assertEqual(production.bom, bom_1)
-        # self.assertEqual(production.routing, routing_1)
-        # self.assertEqual(production.line, line_1)
-        # self.assertEqual(production.work_center, line_1)
-        # self.assertEqual(production.batch_quantity, 5)
-        # self.assertEqual(production.batch_uom, unit)
+            production.on_change_product()
+
+            # self.assertEqual(production.unit, unit)
+            # self.assertEqual(production.bom, bom_1)
+            # self.assertEqual(production.routing, routing_1)
+            # self.assertEqual(production.line, line_1)
+            # self.assertEqual(production.work_center, line_1)
+            # self.assertEqual(production.batch_quantity, 5)
+            # self.assertEqual(production.batch_uom, unit)
 
     @with_transaction()
     def test_check_batch_multiple_on_production(self):
